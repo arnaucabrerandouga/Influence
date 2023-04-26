2a540da6cbdc2c87b5b405bacc92f19192ae50ad
Arnau Cabrera Ndouga <arnau.cabrera.ndouga@estudiantat.upc.edu>
2023-04-26 10:17:05 +0200

    error server.recieve(msg2)

---
+++ Cliente/Cliente/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        int port = 9070;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIniciarSesion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (String.IsNullOrEmpty(textBoxUsuario.Text) || String.IsNullOrEmpty(textBoxContraseña.Text)) // nome she ficat nom o contra  avisame
+                {
+                    MessageBox.Show("Escribe un nombre y una contraseña");
+                }
+                else
+                {
+                    // Creamos un IPEndPoint con el ip del servidor y puerto del sevidor al que conectamos
+                    IPAddress direc = IPAddress.Parse("192.168.56.102");
+                    IPEndPoint ipep = new IPEndPoint(direc, port);
+
+                    //Creamos el socket 
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    server.Connect(ipep);//Intentamos conectar el socket
+                    MessageBox.Show("Conectado");
+
+                    string mensaje = "1/" + textBoxUsuario.Text + "/" + textBoxContraseña.Text;
+                    // Enviamos al servidor el usuario y la contraseña
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    //Recibimos la respuesta del servidor
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+                    if (mensaje == "1/Bien venido") //ha anat be obram un form per fer peticions
+                    {
+                        string Jugador = textBoxUsuario.Text;
+                        // Crear nueva instancia de Form2
+                        Form2 form2 = new Form2(Jugador);
+
+                        // Llamar al método ConectarServidor de Form2 para establecer una nueva conexión
+                        form2.ConectarServidor("192.168.56.102", port);
+
+                        // Mostrar el formulario 2
+                        form2.Show();
+
+
+                    }
+
+
+                    //else if (mensaje == "INCORRECTO")// nom contra incorrecte
+                    //    MessageBox.Show("No se encuentra al usuario/contraseña incorrecta");
+
+                    else if (mensaje == "1/Vaya un error se ha producido")// ERROR
+                        MessageBox.Show("Vaya un error se ha producido");
+
+                    // Se terminó el servicio. 
+                    // Nos desconectamos
+                    this.BackColor = Color.Gray;
+                    //server.Shutdown(SocketShutdown.Both);
+                    //server.Close();
+                    
+                }
+                 
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+
+        }
+
+        private void buttonRegistrarte_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(textBoxUsuario.Text) || String.IsNullOrEmpty(textBoxContraseña.Text)) // nome she ficat nom o contra  avisame
+                {
+                    MessageBox.Show("Escribe un nombre y una contraseña");
+                }
+                else
+                {
+
+
+                    // Creamos un IPEndPoint con el ip del servidor y puerto del sevidor al que conectamos
+                    IPAddress direc = IPAddress.Parse("192.168.56.102");
+                    IPEndPoint ipep = new IPEndPoint(direc, port);
+
+                    //Creamos el socket 
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    server.Connect(ipep);//Intentamos conectar el socket
+                    MessageBox.Show("Conectado");
+
+                    string mensaje = "2/" + textBoxUsuario.Text + "/" + textBoxContraseña.Text;
+                    // Enviamos al servidor el usuario y la contraseña
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    //Recibimos la respuesta del servidor
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+
+                    if (mensaje == "2/Bien venido al club") //ha anat be obram un form per fer peticions
+                        MessageBox.Show("Bien venido al club");
+
+                    //else if (mensaje == "EXISTE") //si el usuario ya existe
+                    //    MessageBox.Show("Este usuario ya existe");
+
+                    else if (mensaje == "2/Vaya un error se ha producido un error")// ERROR
+                        MessageBox.Show("Vaya un error se ha producido un error");
+                   
+                    // Se terminó el servicio. 
+                    // Nos desconectamos
+                    this.BackColor = Color.Gray;
+                    server.Shutdown(SocketShutdown.Both);
+                    server.Close();
+                }
+                
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }         
+        }
+    }
+}
